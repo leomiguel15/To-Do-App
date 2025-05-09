bc45fe8ae7b62a9ebcbf038ef88f55d8caa26296
leonardo_migueljr <leonardo_migueljr@trendmicro.com>
2025-05-09 12:30:47 +0800

    This is the initial program

---
+++ main_app.py
@@ -0,0 +1,28 @@
+
+
+print("Welcome to To-Do app!")
+print("Below is the code format to use the app: \n add | show | exit")
+
+list = []
+
+while True:
+
+    user_action = input("Enter command: ")
+    user_action = user_action.strip().lower()
+
+    match user_action:
+        case "add":
+            var = input("Add your entry: ")
+            if var == "back":
+                continue
+            list.append(var)
+
+        case "show":
+            print("Here's your saved list: ")
+            for items in list:
+                print(items)
+            continue
+        case "exit":
+            break
+
+print("Bye")
