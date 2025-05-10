21d0e22a8e00f4960b5bd13099eda424068660bf
leomiguel15 <lmiguel94104@gmail.com>
2025-05-10 09:00:51 +0800

    Added delete feature

--- main_app.py
+++ main_app.py
@@ -1,24 +1,46 @@
 print("Welcome to To-Do app!")
-print("Below is the code format to use the app: \nadd | show | exit")
+print("---------------------")
+print("Below is the code format to use the app: \nadd | show | delete | exit")
 
 list = []
 
 while True:
 
-    user_action = input("Enter command: ")
+    user_action = input("Enter command:")
     user_action = user_action.strip().lower()
 
     match user_action:
         case "add":
-            var = input("Add your entry: ")
+            var = input("\nAdd your entry: ")
             if var == "back":
                 continue
             list.append(var)
 
         case "show":
-            print("Here's your saved list: ")
+            print("\nHere's your saved list: ")
             for items in list:
                 print("{0}: {1}".format(list.index(items)+1, items))
+            continue
+
+        case "delete":
+            print("\nCurrent notes: ")
+            for items in list:
+                print("{0}: {1}".format(list.index(items)+1, items))
+            
+            #ensure user input < number of notes
+            try:
+                var = int(input("Which note to be deleted (Select by entering the number):"))
+                if 1 <= var <= len(list):
+                    del list[var - 1]
+                    break
+                else:
+                    print(f"Please enter a number between 1 and {len(list)}")
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+
+            print("\nUpdated notes: ")
+            for items in list:
+                print(f"{list.index(items)+1} : {items}")
             continue
 
         case "exit":
